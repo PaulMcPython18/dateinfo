837b3ab484b89fcf71ef323a50813d06ae8b983c
PaulMcPython18 <57735592+PaulMcPython18@users.noreply.github.com>
2020-01-26 13:53:23 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request
 import datetime
-
+from datetime import date
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
 @app.route('/date-time-birthday-calculator')
 def date_time_birthday_calculator():
     return render_template('Date-Info-Birthday-Calculator.html')
+
+
 @app.route('/what-day-of-the-week-am-i-born')
 def whatdayoftheweekwasiborn():
     current_time = datetime.datetime.now()
@@ -20,6 +25,8 @@ def whatdayoftheweekwasiborn():
     current_time = str(current_month) + "/" + str(current_day) + "/" + str(current_year)
     print(current_time)
     return render_template('what-day-of-the-week-was-i-born.html', current_time=current_time)
+
+
 @app.route('/what-day-of-the-week-am-i-born', methods=['POST'])
 def one():
     day = request.form['namequery1']
@@ -28,7 +35,8 @@ def one():
             day = day[1]
             print(day)
         if len(day) > 2:
-            return render_template('what-day-of-the-week-was-i-born.html', message="* Length of month/day must be less than 3 *")
+            return render_template('what-day-of-the-week-was-i-born.html',
+                                   message="* Length of month/day must be less than 3 *")
         day = int(day)
     except:
         return render_template('what-day-of-the-week-was-i-born.html', message="* Do not input words *")
@@ -38,7 +46,8 @@ def one():
             month = month[1]
             print(month)
         if len(month) > 2:
-            return render_template('what-day-of-the-week-was-i-born.html', message="* Length of month/day must be less than 3 *")
+            return render_template('what-day-of-the-week-was-i-born.html',
+                                   message="* Length of month/day must be less than 3 *")
         month = int(month)
     except:
         return render_template('what-day-of-the-week-was-i-born.html', message="* Do not input words *")
@@ -47,7 +56,8 @@ def one():
         year = int(year)
         if int(year) >= 5000 or year < 1000:
             print('Er')
-            return render_template('what-day-of-the-week-was-i-born.html', message="* Year must be less than 5000 and greater than 1000*")
+            return render_template('what-day-of-the-week-was-i-born.html',
+                                   message="* Year must be less than 5000 and greater than 1000*")
     except:
         return render_template('what-day-of-the-week-was-i-born.html', message="* Do not input words *")
     print(day)
@@ -58,12 +68,19 @@ def one():
         classes = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
         print(date)
         print(classes[date.weekday()])
-        return render_template('what-day-of-the-week-was-i-born.html', output=classes[date.weekday()], day = day, month=month, year=year)
+        return render_template('what-day-of-the-week-was-i-born.html', output=classes[date.weekday()], day=day,
+                               month=month, year=year)
     except:
-        return render_template('what-day-of-the-week-was-i-born.html', message='* An error occured! Make sure that you have not inputted a non-existant month, day or year number! *', day = day, month=month, year=year)
+        return render_template('what-day-of-the-week-was-i-born.html',
+                               message='* An error occured! Make sure that you have not inputted a non-existant month, day or year number! *',
+                               day=day, month=month, year=year)
+
+
 @app.route('/how-many-days-have-i-been-alive')
 def how_many_days_have_i_been_alive():
     return render_template('how-many-days-have-i-been-alive.html')
+
+
 @app.route('/how-many-days-have-i-been-alive', methods=['POST'])
 def how_many_days_have_i_lived():
     day = request.form['namequery1']
@@ -72,7 +89,8 @@ def how_many_days_have_i_lived():
             day = day[1]
             print(day)
         if len(day) > 2:
-            return render_template('what-day-of-the-week-was-i-born.html', message="* Length of month/day must be less than 3 *")
+            return render_template('what-day-of-the-week-was-i-born.html',
+                                   message="* Length of month/day must be less than 3 *")
         day = int(day)
     except:
         return render_template('what-day-of-the-week-was-i-born.html', message="* Do not input words *")
@@ -82,9 +100,10 @@ def how_many_days_have_i_lived():
             month = month[1]
             print(month)
         if len(month) > 2:
-            return render_template('what-day-of-the-week-was-i-born.html', message="* Length of month/day must be less than 3 *")
+            return render_template('what-day-of-the-week-was-i-born.html',
+                                   message="* Length of month/day must be less than 3 *")
         month = int(month)
-        
+
     except:
         return render_template('what-day-of-the-week-was-i-born.html', message="* Do not input words *")
     year = request.form['namequery3']
@@ -92,7 +111,8 @@ def how_many_days_have_i_lived():
         year = int(year)
         if int(year) >= 5000 or year < 1000:
             print('Er')
-            return render_template('what-day-of-the-week-was-i-born.html', message="* Year must be less than 5000 and greater than 1000*")
+            return render_template('what-day-of-the-week-was-i-born.html',
+                                   message="* Year must be less than 5000 and greater than 1000*")
     except:
         return render_template('what-day-of-the-week-was-i-born.html', message="* Do not input words *")
     print(day)
@@ -101,15 +121,21 @@ def how_many_days_have_i_lived():
     try:
         today = datetime.date.today()
         bday = datetime.date(year, month, day)
-        output = today-bday
+        output = today - bday
         output = output.days
         output = str(output) + " days!"
-        return render_template('how-many-days-have-i-been-alive.html', output=output, day = day, month=month, year=year)
+        return render_template('how-many-days-have-i-been-alive.html', output=output, day=day, month=month, year=year)
     except:
-        return render_template('how-many-days-have-i-been-alive.html', message='* An error occured! Make sure that you have not inputted a non-existant month, day or year number! *', day = day, month=month, year=year)
+        return render_template('how-many-days-have-i-been-alive.html',
+                               message='* An error occured! Make sure that you have not inputted a non-existant month, day or year number! *',
+                               day=day, month=month, year=year)
+
+
 @app.route('/how-many-minutes-have-i-been-alive')
 def how_many_minutes_have_i_been_alive():
     return render_template('how-many-minutes-have-i-been-alive.html')
+
+
 @app.route('/how-many-minutes-have-i-been-alive', methods=['POST'])
 def how_many_minutes_have_i_lived():
     day = request.form['namequery1']
@@ -118,7 +144,8 @@ def how_many_minutes_have_i_lived():
             day = day[1]
             print(day)
         if len(day) > 2:
-            return render_template('how-many-minutes-have-i-been-alive.html', message="* Length of month/day must be less than 3 *")
+            return render_template('how-many-minutes-have-i-been-alive.html',
+                                   message="* Length of month/day must be less than 3 *")
         day = int(day)
     except:
         return render_template('how-many-minutes-have-i-been-alive.html', message="* Do not input words *")
@@ -128,9 +155,10 @@ def how_many_minutes_have_i_lived():
             month = month[1]
             print(month)
         if len(month) > 2:
-            return render_template('how-many-minutes-have-i-been-alive.html', message="* Length of month/day must be less than 3 *")
+            return render_template('how-many-minutes-have-i-been-alive.html',
+                                   message="* Length of month/day must be less than 3 *")
         month = int(month)
-        
+
     except:
         return render_template('how-many-minutes-have-i-been-alive.html', message="* Do not input words *")
     year = request.form['namequery3']
@@ -138,7 +166,8 @@ def how_many_minutes_have_i_lived():
         year = int(year)
         if int(year) >= 5000 or year < 1000:
             print('Er')
-            return render_template('how-many-minutes-have-i-been-alive.html', message="* Year must be less than 5000 and greater than 1000*")
+            return render_template('how-many-minutes-have-i-been-alive.html',
+                                   message="* Year must be less than 5000 and greater than 1000*")
     except:
         return render_template('how-many-minutes-have-i-been-alive.html', message="* Do not input words *")
     print(day)
@@ -147,16 +176,23 @@ def how_many_minutes_have_i_lived():
     try:
         today = datetime.date.today()
         bday = datetime.date(year, month, day)
-        output = today-bday
+        output = today - bday
         output = output.days
         output = output * 1440
         output = str(output) + " minutes!"
-        return render_template('how-many-minutes-have-i-been-alive.html', output=output,day =day, month=month, year=year)
+        return render_template('how-many-minutes-have-i-been-alive.html', output=output, day=day, month=month,
+                               year=year)
     except:
-         return render_template('how-many-minutes-have-i-been-alive.html', message='* An error occured! Make sure that you have not inputted a non-existant month, day or year number! *', day = day, month=month, year=year)
+        return render_template('how-many-minutes-have-i-been-alive.html',
+                               message='* An error occured! Make sure that you have not inputted a non-existant month, day or year number! *',
+                               day=day, month=month, year=year)
+
+
 @app.route('/how-many-seconds-have-i-been-alive')
 def how_many_seconds_have_i_been_alive():
     return render_template('how-many-seconds-have-i-been-alive.html')
+
+
 @app.route('/how-many-seconds-have-i-been-alive', methods=['POST'])
 def how_many_seconds_have_i_lived():
     day = request.form['namequery1']
@@ -165,7 +201,8 @@ def how_many_seconds_have_i_lived():
             day = day[1]
             print(day)
         if len(day) > 2:
-            return render_template('how-many-seconds-have-i-been-alive.html', message="* Length of month/day must be less than 3 *")
+            return render_template('how-many-seconds-have-i-been-alive.html',
+                                   message="* Length of month/day must be less than 3 *")
         day = int(day)
     except:
         return render_template('how-many-seconds-have-i-been-alive.html', message="* Do not input words *")
@@ -175,7 +212,8 @@ def how_many_seconds_have_i_lived():
             month = month[1]
             print(month)
         if len(month) > 2:
-            return render_template('how-many-seconds-have-i-been-alive.html', message="* Length of month/day must be less than 3 *")
+            return render_template('how-many-seconds-have-i-been-alive.html',
+                                   message="* Length of month/day must be less than 3 *")
         month = int(month)
     except:
         return render_template('how-many-seconds-have-i-been-alive.html', message="* Do not input words *")
@@ -184,7 +222,9 @@ def how_many_seconds_have_i_lived():
         year = int(year)
         if int(year) >= 5000 or year < 1000:
             print('Er')
-            return render_template('how-many-seconds-have-i-been-alive.html', message="* Year must be less than 5000 and greater than 1000*", day=day, month=month, year=year)
+            return render_template('how-many-seconds-have-i-been-alive.html',
+                                   message="* Year must be less than 5000 and greater than 1000*", day=day, month=month,
+                                   year=year)
     except:
         return render_template('how-many-seconds-have-i-been-alive.html', message="* Do not input words *")
     print(day)
@@ -193,14 +233,18 @@ def how_many_seconds_have_i_lived():
     # try:
     today = datetime.date.today()
     bday = datetime.date(year, month, day)
-    output = today-bday
+    output = today - bday
     output = output.days
     output = output * 84600
     output = str(output) + " seconds!"
-    return render_template('how-many-seconds-have-i-been-alive.html', output=output,day =day, month=month, year=year)
+    return render_template('how-many-seconds-have-i-been-alive.html', output=output, day=day, month=month, year=year)
+
+
 @app.route('/how-many-times-your-heart-has-beaten')
 def how_many_times_your_heart_has_beaten():
     return render_template('how-many-times-your-heart-has-beaten.html')
+
+
 @app.route('/how-many-times-your-heart-has-beaten', methods=['POST'])
 def how_many_times_your_heart_has_beat():
     day = request.form['namequery1']
@@ -209,7 +253,8 @@ def how_many_times_your_heart_has_beat():
             day = day[1]
             print(day)
         if len(day) > 2:
-            return render_template('how-many-times-your-heart-has-beaten.html', message="* Length of month/day must be less than 3 *")
+            return render_template('how-many-times-your-heart-has-beaten.html',
+                                   message="* Length of month/day must be less than 3 *")
         day = int(day)
     except:
         return render_template('how-many-times-your-heart-has-beaten.html', message="* Do not input words *")
@@ -219,7 +264,8 @@ def how_many_times_your_heart_has_beat():
             month = month[1]
             print(month)
         if len(month) > 2:
-            return render_template('how-many-times-your-heart-has-beaten.html', message="* Length of month/day must be less than 3 *")
+            return render_template('how-many-times-your-heart-has-beaten.html',
+                                   message="* Length of month/day must be less than 3 *")
         month = int(month)
     except:
         return render_template('how-many-times-your-heart-has-beaten.html', message="* Do not input words *")
@@ -228,23 +274,59 @@ def how_many_times_your_heart_has_beat():
         year = int(year)
         if int(year) >= 5000 or year < 1000:
             print('Er')
-            return render_template('how-many-times-your-heart-has-beaten.html', message="* Year must be less than 5000 and greater than 1000*")
+            return render_template('how-many-times-your-heart-has-beaten.html',
+                                   message="* Year must be less than 5000 and greater than 1000*")
     except:
         return render_template('how-many-times-your-heart-has-beaten.html', message="* Do not input words *")
 
     try:
         today = datetime.date.today()
         bday = datetime.date(year, month, day)
-        output = today-bday
+        output = today - bday
         output = output.days
         output = output * 1440
         output = output * 70
         output = str(output) + " times!"
-        return render_template('how-many-times-your-heart-has-beaten.html', output=output,day =day, month=month, year=year)
+        return render_template('how-many-times-your-heart-has-beaten.html', output=output, day=day, month=month,
+                               year=year)
     except:
-        return render_template('how-many-times-your-heart-has-beaten.html', day=day, month=month, year=year, message='* An error occured, make sure that the date you have inputted actually exists! *')
+        return render_template('how-many-times-your-heart-has-beaten.html', day=day, month=month, year=year,
+                               message='* An error occured, make sure that the date you have inputted actually exists! *')
+
+@app.route('/how-much-food-have-i-eaten-in-my-life-calculator')
+def routeit():
+    return render_template('how-much-food-have-i-eaten-in-my-life.html')
+@app.route('/how-much-food-have-i-eaten-in-my-life-calculator', methods=['POST'])
+def routeit2():
+    difference = 1
+    if 1 == 1:
+        # date1 = day date2 = month date3 = year
+        # format = (year, month, day)
+        date1 = request.form['date1']
+        date2 = request.form['date2']
+        date3 = request.form['date3']
+        if len(date1) > 10 or len(date2) > 10 or len(date3) > 10:
+            return render_template('how-much-food-have-i-eaten-in-my-life.html', message='Whoops! You entered a date that does not exist!')
+
+        difference = datetime.date.today() - datetime.date(int(date3), int(date2), int(date1))
+
+        difference = str(difference)
+        final_difference = ""
+        for char in difference:
+            if char == " ":
+                break
+            else:
+                final_difference += char
+        print(final_difference)
+        difference = int(final_difference) * 4
+        difference = str(difference) + " food pounds."
+        return render_template('how-much-food-have-i-eaten-in-my-life.html', difference = difference, day = date1, month = date2, year = date3)
+    else:
+        return render_template('how-much-food-have-i-eaten-in-my-life.html', message='Whoops! Make sure the date you entered actually exists!')
 @app.route('/sitemap')
 def sitemap():
     return render_template('sitemap.xml')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
